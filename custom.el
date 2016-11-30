934c99a136fbfe266e568dc1fa25debac87075c6
Callum White <callum.white@rea-group.com>
2016-11-30 13:19:49 +1100

    Remove cursor color forcing from custom.el

--- custom.el
+++ custom.el
@@ -103,5 +103,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:height 140 :family "Source Code Pro"))))
- '(cursor ((t (:background "dark gray" :foreground "#fdf6e3" :inverse-video t)))))
+ '(default ((t (:height 140 :family "Source Code Pro")))))
