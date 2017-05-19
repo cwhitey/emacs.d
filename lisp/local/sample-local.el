787ccbecbd562bd6dbf79bbdde0ca51a349d5095
cwhitey <callumw1991@gmail.com>
2017-05-19 15:00:03 +1000

    Add a sample file for a user's local.el

---
+++ lisp/local/sample-local.el
@@ -0,0 +1,11 @@
+;;; Sample file for a `local.el' you might write
+
+;;; local.el --- Local machine configuration
+
+(message "Loading local config")
+
+;; Paradox github integration
+(setq paradox-github-token "some-token")
+
+(provide 'local)
+;;; local.el ends here
